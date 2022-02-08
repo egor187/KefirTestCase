8d57677d29573d7c7da6846b39fe589372018c0f
egor187 <py.egor.py@gmail.com>
2022-02-08 21:57:00 +0300

    added "current" url for user

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from users import views
+
+app_name = "users"
+
+urlpatterns = [
+    # path('', views, name='users'),
+    path('current/', views.CommonUserDetailView.as_view(), name='current_user_common'),
+    # path('<int:pk>/', views, name='patch_user'),
+    # path('private/users/', views, name='private_users'),
+    # path('private/users/<int:pk>/', views, name='private_current_user'),
+]
